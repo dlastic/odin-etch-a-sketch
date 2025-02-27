c876a412c34146f8a378ae31e05c48728bf1b8f7
dlastic <daniellastic@gmail.com>
2025-02-27 16:26:43 +0100

    Add color changing effect when moving the mouse

--- index.js
+++ index.js
@@ -7,8 +7,28 @@ const containerDiv = document.querySelector(".container");
 document.documentElement.style.setProperty("--container-size", `${containerSize}px`);
 document.documentElement.style.setProperty("--square-size", `${squareSize}px`);
 
+// Create grid of square divs
 for (let i = 0; i < gridSize ** 2; i++) {
   const newDiv = document.createElement("div");
   newDiv.classList.add("square");
-  containerDiv.appendChild(newDiv)
+  containerDiv.appendChild(newDiv);
 }
+
+// Change div color when the mouse is down and moves over it
+let isMouseDown = false;
+const squareColor = "blue";
+
+document.addEventListener("mousedown", () => isMouseDown = true);
+document.addEventListener("mouseup", () => isMouseDown = false);
+
+const squareDivs = document.querySelectorAll(".square");
+squareDivs.forEach((square) => {
+    square.addEventListener("mousedown", () => {
+      square.style.backgroundColor = squareColor;
+    })
+    square.addEventListener("mousemove", () => {
+      if (isMouseDown) {
+        square.style.backgroundColor = squareColor;
+      }
+    });
+});
