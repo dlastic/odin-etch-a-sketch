76b5a241de754311acbad73cee92639c9dd28578
dlastic <daniellastic@gmail.com>
2025-02-27 19:22:59 +0100

    Add button for changing the n of squares per side

--- index.js
+++ index.js
@@ -1,34 +1,52 @@
-const gridSize = 10;
-let containerSize = 700;
-const squareSize = Math.round(containerSize / gridSize);
-containerSize = gridSize * squareSize
-const containerDiv = document.querySelector(".container");
+let squaresPerSide = 10;
+const gridContainerSize = 600;
+let isMouseDown = false;
+const containerDiv = document.querySelector(".grid-container");
+const gridSizeBtn = document.querySelector("#grid-size");
+const squareColor = "black";
 
-document.documentElement.style.setProperty("--container-size", `${containerSize}px`);
-document.documentElement.style.setProperty("--square-size", `${squareSize}px`);
-
-// Create grid of square divs
-for (let i = 0; i < gridSize ** 2; i++) {
-  const newDiv = document.createElement("div");
-  newDiv.classList.add("square");
-  containerDiv.appendChild(newDiv);
+function generateGrid(size, squareCount) {
+  const squareSize = size / squareCount;
+  document.documentElement.style.setProperty("--container-size", `${gridContainerSize}px`);
+  document.documentElement.style.setProperty("--square-size", `${squareSize}px`);
+  for (let i = 0; i < squaresPerSide ** 2; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("square");
+    containerDiv.appendChild(newDiv);
+  }
+  setColoringEffect();
 }
 
-// Change div color when the mouse is down and moves over it
-let isMouseDown = false;
-const squareColor = "blue";
+function removeGrid() {
+  while (containerDiv.firstChild) {
+    containerDiv.removeChild(containerDiv.firstChild);
+  }
+}
 
-document.addEventListener("mousedown", () => isMouseDown = true);
-document.addEventListener("mouseup", () => isMouseDown = false);
-
-const squareDivs = document.querySelectorAll(".square");
-squareDivs.forEach((square) => {
-    square.addEventListener("mousedown", () => {
-      square.style.backgroundColor = squareColor;
-    })
-    square.addEventListener("mousemove", () => {
-      if (isMouseDown) {
+function setColoringEffect() {
+  const squareDivs = document.querySelectorAll(".square");
+  document.addEventListener("mousedown", () => isMouseDown = true);
+  document.addEventListener("mouseup", () => isMouseDown = false);
+  squareDivs.forEach((square) => {
+      square.addEventListener("mousedown", () => {
         square.style.backgroundColor = squareColor;
-      }
-    });
-});
+      })
+      square.addEventListener("mousemove", () => {
+        if (isMouseDown) {
+          square.style.backgroundColor = squareColor;
+        }
+      });
+  });
+}
+
+// Generate the starting grid
+generateGrid(gridContainerSize, squaresPerSide);
+
+// Change the grid size with a button
+gridSizeBtn.addEventListener("click", () => {
+  do {
+    squaresPerSide = parseInt(prompt("Enter the number of squares per side (between 1 and 100)"), 10);
+  } while (isNaN(squaresPerSide) || squaresPerSide < 1 || squaresPerSide > 100);
+  removeGrid();
+  generateGrid(gridContainerSize, squaresPerSide);
+})
