538eb63ec25729f49a0537e3d3f0df87bb8f865a
dlastic <daniellastic@gmail.com>
2025-02-28 19:38:45 +0100

    Add rainbow mode and clear button

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-let squaresPerSide = 10;
+let squaresPerSide = 20;
 const gridContainerSize = 600;
 let isMouseDown = false;
+let rainbowMode = false;
 const containerDiv = document.querySelector(".grid-container");
 const gridSizeBtn = document.querySelector("#grid-size");
-const squareColor = "black";
+const rainbowModeBtn = document.querySelector("#rainbow-mode");
+const clearGridBtn = document.querySelector("#clear");
 
 function generateGrid(size, squareCount) {
   const squareSize = size / squareCount;
   document.documentElement.style.setProperty("--container-size", `${gridContainerSize}px`);
   document.documentElement.style.setProperty("--square-size", `${squareSize}px`);
+
   for (let i = 0; i < squaresPerSide ** 2; i++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("square");
@@ -23,17 +26,36 @@ function removeGrid() {
   }
 }
 
+function clearGrid() {
+  document.querySelectorAll(".square").forEach((square) => {
+    square.style.removeProperty("background-color");
+  })
+}
+
+function setColor() {
+  if (rainbowMode) {
+    const rgbRed = Math.floor(Math.random() * 255);
+    const rgbGreen = Math.floor(Math.random() * 255);
+    const rgbBlue = Math.floor(Math.random() * 255);
+    return `rgb(${rgbRed}, ${rgbGreen}, ${rgbBlue})`;
+  } else {
+    return "red";
+  }
+
+}
+
 function setColoringEffect() {
   const squareDivs = document.querySelectorAll(".square");
   document.addEventListener("mousedown", () => isMouseDown = true);
   document.addEventListener("mouseup", () => isMouseDown = false);
+
   squareDivs.forEach((square) => {
       square.addEventListener("mousedown", () => {
-        square.style.backgroundColor = squareColor;
+        square.style.backgroundColor = setColor();
       })
-      square.addEventListener("mousemove", () => {
+      square.addEventListener("mouseover", () => {
         if (isMouseDown) {
-          square.style.backgroundColor = squareColor;
+          square.style.backgroundColor = setColor();
         }
       });
   });
@@ -49,4 +71,16 @@ gridSizeBtn.addEventListener("click", () => {
   } while (isNaN(squaresPerSide) || squaresPerSide < 1 || squaresPerSide > 100);
   removeGrid();
   generateGrid(gridContainerSize, squaresPerSide);
+});
+
+// Rainbow button
+rainbowModeBtn.addEventListener("click", () => {
+  rainbowModeBtn.classList.toggle("rainbow");
+  rainbowMode = !rainbowMode;
+  setColoringEffect();
+});
+
+// Clear button
+clearGridBtn.addEventListener("click", () => {
+  clearGrid();
 })
